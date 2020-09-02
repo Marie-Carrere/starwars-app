5fa59624e3147ddb2ecc36831695d4cd28c03689
Marie-Carrere <marie.carrere@icloud.com>
2020-09-02 22:34:52 +0200

    :construction: WIP Planets component

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets");
+  return res.json();
+}
 
 const Planets = () => {
+  const {data, status} = useQuery("planets", fetchPlanets);
+  console.log(data);
   return (
-    <div>
-      <h2>Planets</h2>
+    <div> 
+      <h2>Planets</h2> 
     </div>
   );
 }
