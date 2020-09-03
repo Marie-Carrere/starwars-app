9701b4495625549c8ceb30824bee699b5f203510
Marie-Carrere <marie.carrere@icloud.com>
2020-09-03 10:15:42 +0200

    :sparkles: Add Person component

--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./Person";
 
-const People = () => {
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people");
+  return res.json();
+}
+
+const Planets = () => {
+  const { data, status } = useQuery("people", fetchPeople);
+
   return (
     <div>
       <h2>People</h2>
+      { status === "success" && (
+        <div>
+          {data.results.map(person => <Person key={person.name} person={person} />)}
+        </div>
+      )}
+
     </div>
   );
 }
 
-export default People;
+export default Planets;
